a8a1851b7b31cd71106db7b6130ac2536da9e334
Fernando Vargas <fvargas320@gmail.com>
2023-02-20 23:36:13 -0800

    Added Retrofit and Gson dependencie

--- app/src/main/java/com/COMP583/Drinkly/Home.kt
+++ app/src/main/java/com/COMP583/Drinkly/Home.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -18,15 +19,23 @@ private const val ARG_PARAM2 = "param2"
  */
 class Home : Fragment() {
     // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
+    private lateinit var newRecyclerView : RecyclerView
+    private lateinit var newArrayList : ArrayList<Cocktails>
+    lateinit var imageID : Array<Int>
+    lateinit var cocktailName : String
+    lateinit var cocktailDesc : String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+
+        cocktailName  = "Margarita"
+        cocktailDesc = "ahblahalsldhs"
+
+    }
+
+    private fun setContentView() {
+        TODO("Not yet implemented")
     }
 
     override fun onCreateView(
@@ -37,23 +46,4 @@ class Home : Fragment() {
         return inflater.inflate(R.layout.fragment_home, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment Home.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            Home().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
 }
